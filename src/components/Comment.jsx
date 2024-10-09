ca3cce8ef0f5774ba921c0c91af1b8009aa6e092
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-10-09 14:28:40 -0400

    Style comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import formatDate from '../formatDate';
+import styles from '../style/Comment.module.css';
 
 function Comment({ comment }) {
   return (
-    <div>
-      <Link to={`/users/${comment.userId}`}>
-        <img className='pfp' src={comment.user.pfpUrl} alt='' />
-        <span>{comment.user.username}</span>
-      </Link>
-      <span>{new Date(comment.timestamp).toLocaleString()}</span>
+    <div className={styles.comment}>
+      <div className={styles.heading}>
+        <Link to={`/users/${comment.userId}`}>
+          <img className='pfp' src={comment.user.pfpUrl} alt='' />
+        </Link>
+        <Link to={`/users/${comment.userId}`}>
+          <strong>{comment.user.username}</strong>
+        </Link>
+        <span className={styles.timestamp}>{formatDate(comment.timestamp)}</span>
+      </div>
       <p>{comment.text}</p>
     </div>
   );
