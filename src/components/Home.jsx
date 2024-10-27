f778be9826f46ab0c3f60cf15449e27cffce445f
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-10-27 16:53:21 -0400

    Move non-form section of NewPostForm back to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, useOutletContext } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
+import GifPicker from 'gif-picker-react';
 import Post from './Post.jsx';
 import NewPostForm from './NewPostForm.jsx';
+import styles from '../style/Home.module.css';
 
 function Home() {
   const [posts, setPosts] = useState(null);
+  const [newPostImagesrc, setNewPostImagesrc] = useState('');
+  const [gifUrl, setGifUrl] = useState('');
+  const modalRef = useRef(null);
   const [setError, currentUser] = useOutletContext();
   const navigate = useNavigate();
 
@@ -34,18 +39,45 @@ function Home() {
     setPosts(newPosts);
   }
 
-  function removePost(postId) {
-    setPosts(posts.filter((post) => post.id !== postId));
-  }
-
   return !currentUser || !posts ? (
     <h1>Loading...</h1>
   ) : (
     <main>
-      <NewPostForm
-        posts={posts}
-        setPosts={(p) => setPosts(p)}
-      />
+      <dialog ref={modalRef}>
+        <button
+          className={styles.closeModalButton}
+          onClick={() => modalRef.current.close()}
+        >
+          <span className='material-symbols-outlined closeButton'>close</span>
+        </button>
+        <div>
+          <GifPicker
+            tenorApiKey={import.meta.env.VITE_TENOR_API_KEY}
+            theme='dark'
+            onGifClick={(selected) => {
+              setGifUrl(selected.url);
+              setNewPostImagesrc('');
+              modalRef.current.close();
+            }}
+          />
+        </div>
+      </dialog>
+      <div className={styles.formSection}>
+        <div className={styles.currentUserPfp}>
+          <Link to={`/users/${currentUser.id}`}>
+            <img className='pfp' src={currentUser.pfpUrl} alt='' />
+          </Link>
+        </div>
+        <NewPostForm
+          modalRef={modalRef}
+          posts={posts}
+          setPosts={(p) => setPosts(p)}
+          newPostImagesrc={newPostImagesrc}
+          gifUrl={gifUrl}
+          setNewPostImagesrc={(src) => setNewPostImagesrc(src)}
+          setGifUrl={(url) => setGifUrl(url)}
+        />
+      </div>
       <div>
         {posts.length === 0 ? (
           <h2 className='nothingHere'>You and your followers have no posts.</h2>
@@ -55,7 +87,9 @@ function Home() {
               key={post.id}
               post={post}
               replacePost={(updatedPost) => replacePost(updatedPost)}
-              removePost={(postId) => removePost(postId)}
+              removePost={(postId) =>
+                setPosts(posts.filter((p) => p.id !== postId))
+              }
             />
           ))
         )}
--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,18 +1,22 @@
 import { useRef, useState } from 'react';
-import { Link, useOutletContext } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import GifPicker from 'gif-picker-react';
 import PollInputs from './PollInputs.jsx';
 import styles from '../style/NewPostForm.module.css';
 
-function NewPostForm({ posts, setPosts }) {
-  const [newPostImagesrc, setNewPostImagesrc] = useState('');
-  const [gifUrl, setGifUrl] = useState('');
+function NewPostForm({
+  modalRef,
+  posts,
+  setPosts,
+  newPostImagesrc,
+  gifUrl,
+  setNewPostImagesrc,
+  setGifUrl,
+}) {
   const [isPoll, setIsPoll] = useState(false);
   const [pollChoiceCount, setPollChoiceCount] = useState(2);
   const fileInputRef = useRef(null);
-  const modalRef = useRef(null);
-  const [setError, currentUser] = useOutletContext();
+  const [setError] = useOutletContext();
 
   function cancelNewPostImage() {
     fileInputRef.current.value = '';
@@ -104,127 +108,100 @@ function NewPostForm({ posts, setPosts }) {
   }
 
   return (
-    <>
-      <dialog ref={modalRef}>
-        <button
-          className={styles.closeModalButton}
-          onClick={() => modalRef.current.close()}
-        >
-          <span className='material-symbols-outlined closeButton'>close</span>
-        </button>
-        <div className={styles.GifPicker}>
-          <GifPicker
-            tenorApiKey={import.meta.env.VITE_TENOR_API_KEY}
-            theme='dark'
-            onGifClick={(selected) => {
-              setGifUrl(selected.url);
-              setNewPostImagesrc('');
-              modalRef.current.close();
-            }}
+    <form
+      className={styles.postForm}
+      encType='multipart/form-data'
+      onSubmit={(e) => (isPoll ? sumbitPoll(e) : submitPost(e))}
+    >
+      <textarea
+        className={styles.postText}
+        name='postText'
+        id='postText'
+        maxLength={1000}
+        placeholder={!isPoll ? 'New Post' : 'Question'}
+        onInput={(e) => {
+          e.target.style.height = 'auto';
+          e.target.style.height = `${e.target.scrollHeight}px`;
+        }}
+        required={!newPostImagesrc && !gifUrl}
+      ></textarea>
+      {isPoll && (
+        <PollInputs
+          pollChoiceCount={pollChoiceCount}
+          setPollChoiceCount={(n) => setPollChoiceCount(n)}
+        />
+      )}
+      {(newPostImagesrc !== '' || gifUrl !== '') && (
+        <div className={styles.imgPreview}>
+          <img
+            src={
+              newPostImagesrc ? URL.createObjectURL(newPostImagesrc) : gifUrl
+            }
+            alt=''
           />
+          <button
+            type='button'
+            className={styles.cancelPreviewButton}
+            onClick={() => cancelNewPostImage()}
+          >
+            <span className='material-symbols-outlined closeButton'>close</span>
+          </button>
         </div>
-      </dialog>
-      <div className={styles.nonModal}>
-        <div className={styles.currentUserPfp}>
-          <Link to={`/users/${currentUser.id}`}>
-            <img className='pfp' src={currentUser.pfpUrl} alt='' />
-          </Link>
-        </div>
-        <form
-          className={styles.postForm}
-          encType='multipart/form-data'
-          onSubmit={(e) => (isPoll ? sumbitPoll(e) : submitPost(e))}
-        >
-          <textarea
-            className={styles.postText}
-            name='postText'
-            id='postText'
-            maxLength={1000}
-            placeholder={!isPoll ? 'New Post' : 'Question'}
-            onInput={(e) => {
-              e.target.style.height = 'auto';
-              e.target.style.height = `${e.target.scrollHeight}px`;
-            }}
-            required={!newPostImagesrc && !gifUrl}
-          ></textarea>
-          {isPoll && (
-            <PollInputs
-              pollChoiceCount={pollChoiceCount}
-              setPollChoiceCount={(n) => setPollChoiceCount(n)}
-            />
-          )}
-          {(newPostImagesrc !== '' || gifUrl !== '') && (
-            <div className={styles.imgPreview}>
-              <img
-                src={
-                  newPostImagesrc
-                    ? URL.createObjectURL(newPostImagesrc)
-                    : gifUrl
-                }
-                alt=''
-              />
-              <button
-                type='button'
-                className={styles.cancelPreviewButton}
-                onClick={() => cancelNewPostImage()}
-              >
-                <span className='material-symbols-outlined closeButton'>
-                  close
-                </span>
-              </button>
-            </div>
-          )}
-          <input
-            type='file'
-            name='postImage'
-            id='postImage'
-            accept='image/*'
-            hidden
-            ref={fileInputRef}
-            onChange={(e) => handleFileInputChange(e)}
-          />
-          <div className={styles.formButtons}>
-            {isPoll ? (
-              <button type='button' onClick={() => setIsPoll(false)}>
-                Cancel Poll
-              </button>
-            ) : (
-              <div className={styles.svgButtons}>
-                <button className={styles.svgButton} type='button'>
-                  <label htmlFor='postImage'>
-                    <span className='material-symbols-outlined'>image</span>
-                  </label>
-                </button>
-                <button
-                  className={styles.svgButton}
-                  type='button'
-                  onClick={() => modalRef.current.showModal()}
-                >
-                  <span className='material-symbols-outlined'>gif_box</span>
-                </button>
-                <button
-                  className={styles.svgButton}
-                  type='button'
-                  onClick={() => {
-                    setIsPoll(true);
-                    cancelNewPostImage();
-                  }}
-                >
-                  <span className='material-symbols-outlined'>ballot</span>
-                </button>
-              </div>
-            )}
-            <button className={styles.submitButton}>Post</button>
+      )}
+      <input
+        type='file'
+        name='postImage'
+        id='postImage'
+        accept='image/*'
+        hidden
+        ref={fileInputRef}
+        onChange={(e) => handleFileInputChange(e)}
+      />
+      <div className={styles.formButtons}>
+        {isPoll ? (
+          <button type='button' onClick={() => setIsPoll(false)}>
+            Cancel Poll
+          </button>
+        ) : (
+          <div className={styles.svgButtons}>
+            <button className={styles.svgButton} type='button'>
+              <label htmlFor='postImage'>
+                <span className='material-symbols-outlined'>image</span>
+              </label>
+            </button>
+            <button
+              className={styles.svgButton}
+              type='button'
+              onClick={() => modalRef.current.showModal()}
+            >
+              <span className='material-symbols-outlined'>gif_box</span>
+            </button>
+            <button
+              className={styles.svgButton}
+              type='button'
+              onClick={() => {
+                setIsPoll(true);
+                cancelNewPostImage();
+              }}
+            >
+              <span className='material-symbols-outlined'>ballot</span>
+            </button>
           </div>
-        </form>
+        )}
+        <button className={styles.submitButton}>Post</button>
       </div>
-    </>
+    </form>
   );
 }
 
 NewPostForm.propTypes = {
+  modalRef: PropTypes.object,
   posts: PropTypes.array,
   setPosts: PropTypes.func,
+  newPostImagesrc: PropTypes.string,
+  gifUrl: PropTypes.string,
+  setNewPostImagesrc: PropTypes.func,
+  setGifUrl: PropTypes.func,
 };
 
 export default NewPostForm;
